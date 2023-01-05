368cade2bc13abe47ebe70c7bd2fd987877b1092
Mizanur Rahman <nickty.86@gmail.com>
2023-01-05 20:55:24 +0600

    Create models users then migrations

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
-
+from .models import User
 # Create your views here.
 @api_view(['GET'])
-def hello(request):
-    return Response('hello')
+def users(request):
+    users = User.objects.all()
+    return Response(users)
