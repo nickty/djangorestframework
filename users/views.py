25b97be29decb2183b93ee1146c890413618adc8
Mizanur Rahman <nickty.86@gmail.com>
2023-01-05 22:01:07 +0600

    Create new user

--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from rest_framework import exceptions 
+from .serializers import UserSerializer
 from .models import User
 # Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    if(data['password']) != data['password_confirm']:
+        raise exceptions.APIException('Password do not match!')
+    serializer = UserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
+
+
 @api_view(['GET'])
 def users(request):
     users = User.objects.all()
